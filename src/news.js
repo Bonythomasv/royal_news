580df4bfb4780279d7f23463c88d48ec80702a79
Bony <bonythomasv@gmail.com>
2018-05-07 17:34:08 -0500

    Update news.js

--- src/news.js
+++ src/news.js
@@ -1,6 +1,6 @@
 // Change YOUR_API_KEY_HERE to your apiKey
 const url =
-  "https://newsapi.org/v2/top-headlines?country=us&apiKey=YOUR_API_KEY_HERE";
+  "https://newsapi.org/v2/top-headlines?country=us&apiKey=f32a4052cc4c4ba7a6853c2ed7304cb1";
 
 export async function getNews() {
   let result = await fetch(url).then(response => response.json());
